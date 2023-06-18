fbc7aa1fec427f937fe607690af1b1abe6d113ac
MROVENSKYI <markrovensky@gmail.com>
2023-06-18 19:39:34 +0300

    work with db + design

--- laracasts_laravel/resources/views/layout.blade.php
+++
@@ -1,8 +0,0 @@
-<!doctype html>
-
-<title>my blog</title>
-<link rel="stylesheet" href="/app.css">
-
-<body>
-@yield('content')
-</body>
--- laracasts_laravel/routes/web.php
+++ laracasts_laravel/routes/web.php
@@ -1,26 +1,45 @@
 <?php
 
-
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
-use Illuminate\Support\Facades\File;
 
-
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
-return view ('posts', [
-    'posts' => Post::all()
+    return view('posts', [
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all()
+    ]);
+})->name('home');
+
+Route::get('posts/{post:slug}', function (Post $post) {
+    return view('post', [
+        'post' => $post
     ]);
 });
-// $document = YamlFrontMatter::parseFile(
-//     resource_path('posts/my-fourth-post.html')
-// );
 
-Route::get('posts/{post}', function ($slug) {
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
+    ]);
+})->name('category');
 
-    return view ('post', [
-        'post' => Post::findOrFail($slug)
-        ]);
-
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts,
+        'categories' => Category::all()
+    ]);
 });
