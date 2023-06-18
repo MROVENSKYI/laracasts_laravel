2dc4f180683fb52c083f0e41749f06b42853acbc
MROVENSKYI <markrovensky@gmail.com>
2023-06-18 19:59:14 +0300

    serch/filtering/pagination

---
+++ laracasts_laravel/app/Http/Controllers/PostController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(
+                        request(['search', 'category', 'author'])
+                    )->paginate(18)->withQueryString()
+        ]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('post', [
+            'post' => $post
+        ]);
+    }
+}
